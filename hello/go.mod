965f9a97dab2a25147e4d0891bb4b359a17474fc
Esau Castillo <ecastillochin@gmail.com>
2021-01-04 16:54:47 -0600

    using 3rd party

--- hello/go.mod
+++ hello/go.mod
@@ -1,3 +1,5 @@
 module github.com/ecastillochin/hello
 
 go 1.15
+
+require rsc.io/quote v1.5.2
--- hello/main.go
+++ hello/main.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 
 	"github.com/ecastillochin/hello/greet"
+
+	"rsc.io/quote"
 )
 
 func main() {
@@ -11,4 +13,5 @@ func main() {
 	fmt.Println(greet.English())
 	fmt.Println(greet.Italian())
 	fmt.Println(greet.Spanish())
+	fmt.Println(quote.Hello())
 }
